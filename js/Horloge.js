bb7a701add195e7b8b35c66824ef1adcefca16c9
Mélissa Morin <melissa.morinmangaillou@gmail.com>
2020-01-17 12:45:15 -0500

    Version 2

--- js/Horloge.js
+++ js/Horloge.js
@@ -11,46 +11,38 @@ export default class Horloge {
         this.aiguille= document.querySelectorAll(".aiguille")
         console.log(this.aiguille)
 
+        this.heure()
         
         this.horlogeFond.addEventListener("click", () => {
             for (let uneAiguille of this.aiguille){
                 uneAiguille.style.backgroundColor = this.couleurAlea()
             }
-            // this.aiguille[0].style.backgroundColor = this.couleurAlea()
-            // this.aiguille[1].style.backgroundColor = this.couleurAlea()
-            // this.aiguille[2].style.backgroundColor = this.couleurAlea()
 
         })
         
         setInterval(() => {
-            this.aiguille[0].style.backgroundColor = this.couleurAlea()
-            let d = new Date();
-            let s = d.getSeconds()
-            let m = d.getMinutes()
-            let h = d.getHours()
-            
-            let degreSeconde = map(s, 0, 59, 0, 360)
-            let degreMinute = map(m, 0, 59, 0, 360)
-            let degreHeure = map(h, 0, 12, 0, 360)
-            // console.log(map(s, 0, 59, 0, 359))
-            this.aiguille[2].style.transform = "rotate(" + degreSeconde + "deg)"
-            this.aiguille[1].style.transform = "rotate(" + degreMinute + "deg)"
-            this.aiguille[0].style.transform = "rotate(" + degreHeure + "deg)"
 
-            // transform: rotate(20deg);
+            this.aiguille[0].style.backgroundColor = this.couleurAlea()
+            this.heure()
+
         },1000)
         
     }
-        static load() {
-            return new Promise(resolve => {
-                window.addEventListener("load", () => {
-                    resolve();
-                });
-            });
-        }
 
-    
-
+    heure() {
+        let d = new Date();
+        let s = d.getSeconds()
+        let m = d.getMinutes()
+        let h = d.getHours()
+        
+        let degreSeconde = map(s, 0, 59, 0, 360)
+        let degreMinute = map(m, 0, 59, 0, 360)
+        let degreHeure = map(h, 0, 12, 0, 360)
+        // console.log(map(s, 0, 59, 0, 359))
+        this.aiguille[2].style.transform = "rotate(" + degreSeconde + "deg)"
+        this.aiguille[1].style.transform = "rotate(" + degreMinute + "deg)"
+        this.aiguille[0].style.transform = "rotate(" + degreHeure + "deg)"
+    }
 
     couleurAlea() {
         var h = Math.floor(Math.random() * 360);
